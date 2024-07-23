ebdf2cae13d292bc67f09833c9068b3d5286c82a
andihasan97 <andihasanelfalakiy@gmail.com>
2024-07-23 08:41:24 +0700

    add: dokka configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,9 +31,15 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     alias(libs.plugins.jvm)
+    // Aplly Dokka plugin
+    id("org.jetbrains.dokka") version "1.9.20"
 
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+}
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
 }
 
 repositories {
